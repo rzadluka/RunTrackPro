61ace3a4f203615c1856aeb3372d4b8997064be3
Vetorim <irigerd@gmail.com>
2023-10-25 21:26:17 -0500

    add home, run, stats button.

--- app/src/main/java/com/cs407/runtrackpro/MainActivity.java
+++ app/src/main/java/com/cs407/runtrackpro/MainActivity.java
@@ -1,8 +1,11 @@
 package com.cs407.runtrackpro;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,48 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager =getSupportFragmentManager();
+        Button HomeFragmentButton =findViewById(R.id.home_button);
+        HomeFragmentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainerView3, HomeFragment.class,null)
+                        .setReorderingAllowed(true)
+                        .addToBackStack("Showing Home")
+                        .commit();
+            }
+        });
+
+        Button RunFragmentButton =findViewById(R.id.plan_button);
+        RunFragmentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainerView3,Fragment1.class,null)
+                        .setReorderingAllowed(true)
+                        .addToBackStack("Showing Fragment1")
+                        .commit();
+
+            }
+        });
+
+        Button StatsFragmentButton =findViewById(R.id.stat_button);
+        StatsFragmentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainerView3,StatsFragment.class,null)
+                        .setReorderingAllowed(true)
+                        .addToBackStack("Showing Fragment1")
+                        .commit();
+
+            }
+        });
+    }
+
+    public void EnabledMilesButtonByDefault(){
+
     }
 }
