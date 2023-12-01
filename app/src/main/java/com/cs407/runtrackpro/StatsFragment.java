41612881439c1da8dab87806c9ab73f249129641
Lukasz Rzadkowski <rzadkowski@wisc.edu>
2023-12-01 17:58:25 -0600

    graph test working

--- app/src/main/java/com/cs407/runtrackpro/StatsFragment.java
+++ app/src/main/java/com/cs407/runtrackpro/StatsFragment.java
@@ -1,12 +1,22 @@
 package com.cs407.runtrackpro;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
+import java.util.ArrayList;
 
 public class StatsFragment extends Fragment {
 
@@ -42,4 +52,58 @@ public class StatsFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_stats, container, false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        LineChart chart = (LineChart) view.findViewById(R.id.chart);
+
+        LineData data = getData(36, 100);
+        setupChart(chart, data, Color.rgb(200, 200, 200));
+//        chart.setData(data); chart.invalidate();
+    }
+
+    private LineData getData(int count, float range) {
+
+        ArrayList<Entry> values = new ArrayList<>();
+
+        for (int i = 0; i < count; i++) {
+            float val = (float) (Math.random() * range) + 3;
+            values.add(new Entry(i, val));
+        }
+
+        // create a dataset and give it a type
+        LineDataSet set1 = new LineDataSet(values, "DataSet 1");
+//         set1.setFillAlpha(110);
+//         set1.setFillColor(Color.RED);
+
+        set1.setLineWidth(1.75f);
+        set1.setCircleRadius(5f);
+        set1.setCircleHoleRadius(2.5f);
+        set1.setColor(Color.WHITE);
+        set1.setCircleColor(Color.MAGENTA);
+//        set1.setHighLightColor(Color.WHITE);
+//        set1.setDrawValues(false);
+
+        // create a data object with the data sets
+        return new LineData(set1);
+    }
+
+    private void setupChart(LineChart chart, LineData data, int color) {
+        // enable touch gestures
+        chart.setTouchEnabled(true);
+        // enable scaling and dragging
+        chart.setDragEnabled(true);
+        chart.setScaleEnabled(true);
+        chart.setPinchZoom(false);
+
+        chart.getDescription().setEnabled(false);
+        chart.setBackgroundColor(color);
+
+        // add data
+        chart.setData(data);
+
+        // animate calls invalidate()...
+        chart.animateX(2500);
+    }
+
 }
